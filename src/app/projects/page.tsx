bdc0b8651fe8b102e4ec14724f6d2af72c926e30
tahiamubin <tahia.mubin1@gmail.com>
2026-08-17 20:26:11 +0600

    projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,7 +6,7 @@ const page = async() => {
   const user =await getUser()
   console.log(user)
   const projects = await getProjectById(user!.id)
-  console.log(projects)
+  //  console.log(projects)
   return (
     <div className="min-h-screen bg-black p-6">
       <h1 className="text-3xl font-bold text-white mb-2">MY PROJECTS</h1>
